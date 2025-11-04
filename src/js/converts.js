4d0f347f0e9beb619fb05ce7583c541eba1a1e8f
kingminor <michaelgbrightman@yahoo.com>
2025-11-04 08:35:52 -0700

    Update converts.js

--- src/js/converts.js
+++ src/js/converts.js
@@ -11,3 +11,17 @@ export function weightToKilograms(weight) {
     }
     return Number((weight * 0.1).toFixed(2));
 }
+
+export function heightToMeters(height) {
+    if (typeof height != 'number') {
+        throw new Error("Input must be a number");
+    }
+    return Number((height / 10).toFixed(2));
+}
+
+export function heightToFeet(height) {
+    if (typeof height != 'number') {
+        throw new Error("Input must be a number");
+    }
+    return Number((height * 0.328084).toFixed(2));
+}
